b72f3f657a64804a0872aea675a7a90de7198df4
zangsu <zangsu_@naver.com>
2022-05-23 11:20:02 +0900

    create blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Card
 
 # Create your views here.
+def index(request):
+    cards = Card.objects.all()
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'cards': cards,
+        }
+    )
