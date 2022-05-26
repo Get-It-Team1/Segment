57008616ea1a999ebc47a4635c17b20b85b9cd88
yun8565 <yunyun8565@naver.com>
2022-05-26 22:05:59 +0900

    로그인 기능 구현

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('<int:pk>/', views.ReviewDetail.as_view()),
     path('Experience/', views.ExperienceList),
     path('create_post/', views.PostCreate.as_view()),
+    
 ]
--- segment_Rlead_prj/settings.py
+++ segment_Rlead_prj/settings.py
@@ -129,3 +129,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+LOGIN_REDIRECT_URL = '/'
