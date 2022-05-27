4347d967bec780589b57aaf7581a1ccb1e261124
yun8565 <yunyun8565@naver.com>
2022-05-27 14:37:37 +0900

    미디어 경로 업데이트 및 리스트 페이지 수정

--- segment_Rlead_prj/settings.py
+++ segment_Rlead_prj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,6 @@ SITE_ID = 1
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 LOGIN_REDIRECT_URL = '/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
